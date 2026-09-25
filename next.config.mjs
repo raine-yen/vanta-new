7f74e5cbe66ffac250e5745da26fb204f3279af0
raine-yen <raine_yen@outlook.com>
2026-09-24 17:49:49 -0700

    fix: add webpack alias for @ path in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: { allowedOrigins: process.env.ALLOWED_ORIGINS?.split(",") || ["localhost:3000"] },
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
   },
   async headers() {
     return [
